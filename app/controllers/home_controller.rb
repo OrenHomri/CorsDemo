5a5bcdb5aa9a57094fe810a159b86595af810059
Avi K <avi1234@gmail.com>
2014-02-17 13:35:22 +0200

    add cors to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,20 @@
 class HomeController < ApplicationController
-  def Index
-  end
+  
+	after_action :allow_cors
 
-  def Get
-  	render :json => '{"Total":0,"Failed":0,"Passed":0,"Remaining":0,"FailedPercent":-2147483648,"PassedPercent":-2147483648,"RemainingPercent":-2147483648}'
-  end
+    private
+
+    def allow_cors
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = '*'
+      headers['Access-Control-Max-Age'] = "1728000"
+    end
+
+  	def Index
+  	end
+
+  	def Get
+  		render :json => '{"Total":0,"Failed":0,"Passed":0,"Remaining":0,"FailedPercent":-2147483648,"PassedPercent":-2147483648,"RemainingPercent":-2147483648}'
+  	end
 end
